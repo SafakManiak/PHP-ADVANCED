4e6d60ff2732d73e2239fe23b4804f47643e6c17
= <=>
2021-05-20 13:03:22 +0100

    ik kon geen list maken

---
+++ 1-Arrays/taak01/Associatieve.php
@@ -0,0 +1,11 @@
+
+
+<?php
+
+$lengte ['piet'] = 187;
+$lengte [ 'henk'] = 164;
+$lengte ['steen']= 183;
+
+
+echo 'mijn lengte is '. $lengte ['steen']
+?>
---
+++ 1-Arrays/taak02/loops-while.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$leeftijd = 0;
+
+while( $leeftijd <= 17){
+
+    echo " ik ben $leeftijd jaar ,dus ik mag nog niet stemmen <br>";
+
+$leeftijd++;
+
+}
+
+    echo "ik ben $leeftijd oud dus ik heb stemrecht";
+    
+
+
+?>
+
+
