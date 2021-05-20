c145094270c4cde814acf4716f689935c003045e
= <=>
2021-05-20 13:34:18 +0100

    gereed

---
+++ 2-Loops/taak02/for.php
@@ -0,0 +1,13 @@
+<?php
+
+
+for ($saaf = 0; $saaf <= 17;$saaf++ ){
+
+   echo "ik ben  $saaf jaar, dus ik mag nog niet stemmen <br/>"; 
+
+
+}
+
+echo "ik ben $saaf jaar oud ik heb stemrecht!"
+
+?>
