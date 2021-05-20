073171dd6bd55252d87d5d0e35c860bf567c2d94
= <=>
2021-05-20 13:51:02 +0100

    met een unordered list

---
+++ 2-Loops/taak03/forreachdeel1.php
@@ -0,0 +1,16 @@
+<?php
+
+$dagen = array("maandag", "dinsadg", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag");
+
+
+foreach( $dagen as $value ) {
+    ECHO $value. "IS EEN DAG <li/>";
+
+
+}; 
+
+
+?>
+
+
+
