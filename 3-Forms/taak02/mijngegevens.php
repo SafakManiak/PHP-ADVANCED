05cdbb5ac0ad5c2871b80d983f61e5d905a464a7
= <=>
2021-05-25 20:51:37 +0200

    BG color gefixt

--- 3-Forms/taak02/mijngegevens.php
+++ 3-Forms/taak02/mijngegevens.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            background-color: <?php echo $_POST["kleur"]; ?>
+        }
+    </style>
+</head>
+<body>
 <?php
 
 echo $_POST["geslacht"];
@@ -7,3 +21,6 @@ echo $_POST["kleur"];
 
 
 ?>
+
+</body>
+</html>
