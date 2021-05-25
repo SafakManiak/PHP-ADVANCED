0e2d7267a4aff643009f850605cd6c947dddfef2
= <=>
2021-05-25 13:08:24 +0200

    prima

---
+++ 2-Loops/taak05/mijngegevens.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+    <?php
+echo ""  .$_POST["invoerformulier"];
+
+
+    ?>
+
+    
+</body>
+</html>
+
+
+
